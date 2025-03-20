4eb0590fb171fd9864529ad3817b0f3a02880b8a
caoding <caoding.xjx@alibaba-inc.com>
2025-03-20 17:50:14 +0800

    增加了生成业务链路代码单元测试的能力

--- llm-stability-link/src/main/java/com/stability/link/config/ChatClientConfig.java
+++ llm-stability-link/src/main/java/com/stability/link/config/ChatClientConfig.java
@@ -38,20 +38,21 @@ public class ChatClientConfig {
     public ChatClient chatClient(ChatClient.Builder modelBuilder, VectorStore vectorStore, ChatMemory chatMemory) {
 
         return modelBuilder.defaultSystem(
-                "您是一个资深的JAVA开发程序员，能够根据用户输入的类和方法简称，按照下面的步骤，产出完整的调用链路。\n"
+                "您是一个资深的JAVA开发程序员，能够根据用户输入的类和方法简称，按照下面的步骤，产出完整的调用链路，并生成单元测试代码。\n"
                     + "  1、根据用户输入的类和方法简称，在本项目（当前java进程中）先判断类是否存在，如果存在找到对应的方法\n"
-                    +"<方法解析示例>\n"
-                    + "类似用户输入：com.stability.link.client.AssistantController类的chat方法，能够拆接触类名：com.stability.link.client.AssistantController和方法名chat\n"
-                    +"</方法解析示例>\n"
-                    + "  2、梳理整个方法和方法的内部调用的代码，如果返回的类和方法在当前项目工程中还存在，那就继续往下一层递归，最终返回业务链路调用栈。\n"
+                    + "<方法解析示例>\n"
+                    + "类似用户输入：com.stability.link.client.AssistantController类的chat方法，能够拆接触类名：com.stability.link.client"
+                    + ".AssistantController和方法名chat\n"
+                    + "</方法解析示例>\n"
+                    + "  2、梳理整个方法和方法的内部调用的代码，如果当前方法内部依赖的类和方法在本项目（当前java"
+                    + "进程中）依旧存在，那就重复当前流程，用被依赖的类、方法继续向下梳理依赖，最终返回业务链路相关的调用栈。\n"
                     + "特别要注意：过滤掉标准非业务相关的工具类，这是重点。\n"
                     + "最后返回的结果大概如下：\n"
-                    + "<调用栈结果实例>\n"
-                    + "  com.stability.link.client.BookingController类的getBookings方法的完整路径\n"
+                    + "<调用栈结果示例>\n"
                     + "  com.stability.link.client.BookingController#getBookings\n"
                     + "    com.stability.link.services.FlightBookingService.getBookings\n"
                     + "      com.stability.link.repo.BookingDataRepo.query\n"
-                    + "</调用栈结果实例>\n"
+                    + "</调用栈结果示例>\n"
                     + "  3、根据方法调用链路，生成uml时序图的代码\n"
                     + "<时序图代码示例>\n"
                     + "@startuml\n"
@@ -69,7 +70,7 @@ public class ChatClientConfig {
                     + "deactivate BC\n"
                     + "@enduml"
                     + "</时序图代码示例>\n"
-
+                    + "  4、根据业务链路相关的调用栈，生成所有业务链路上被调用方法的单元测试代码。\n"
             ).
             defaultAdvisors(
                 // Chat Memory
